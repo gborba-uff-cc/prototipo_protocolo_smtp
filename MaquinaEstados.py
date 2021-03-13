d7d788ae44d6d7935c23e9bf2f943b1dedfcd199
Gabriel Borba <gborba@id.uff.br>
2021-03-13 12:20:35 -0300

    declarando as classes necessárias à maquina de estados

---
+++ MaquinaEstados.py
@@ -0,0 +1,11 @@
+class MaquinaEstados():
+    def __init__(self, estadoInicial, estadoFinal):
+        pass
+
+class EstadosMaquina():
+    def __init__(self,
+        funcaoAoEntrar      = lambda *args, **kwargs : 0,
+        funcaoAoExecutar    = lambda *args, **kwargs : 0,
+        funcaoAoSair        = lambda *args, **kwargs : 0
+    ):
+        pass
