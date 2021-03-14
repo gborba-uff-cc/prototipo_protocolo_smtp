da50ee4bed402e621c1295c974aee120f5d0fa4f
Gabriel Borba <gborba@id.uff.br>
2021-03-13 21:23:33 -0300

    reecrita do código e acrescimo de TODOs

--- server_stmp.py
+++ server_stmp.py
@@ -7,112 +7,86 @@ if __name__ != '__main__':
 
 # ============================================
 def processaConexao(sConexao : socket.socket):
+    NOME_APRESENTACAO = 'stmp.prototipo'
+    TAM_BUFFER_RECV = 1024
     mensagemBytes = bytes(())
+    mensagem = ''
     mensagemTokens = []
+    quantidadeTokens = 0
     comandoRecebido = ''
     erro = False
     terminouConexao = False
     idCliente = ''
     emailRemetente = ''
     emailDestinatario = ''
+    # faz que o comando HELO aceito apenas como o primeiro comando
+    clienteIdentificado = False
+    # mantem a ordem para: MAIL FROM: -> RCPT TO: -> DATA
+    ordemComando = 0
+    # faz que comando QUIT seja aceito ao enviar no minimo uma mensagem
+    mensagemRecebida = False
 
     # 220 <...> ================================================================
-    # envia mensagem
-    sConexao.send('220 stmp.prototype'.encode('UTF8'))
+    # envia mensagem boas vindas
+    sConexao.send('220 {}'.format(NOME_APRESENTACAO).encode('UTF8'))
 
-    # HELO <...> ===============================================================
-    # recebe mensagem
-    mensagemBytes = sConexao.recv(1024)
-    # decodifica bytes recebidos
-    mensagemTokens = mensagemBytes.decode('UTF8').split()
-    # pega comando
-    comandoRecebido = mensagemTokens[0]
-    # verifica inconsistencia e se o comando passado foi HELO <...>
-    erro = len(mensagemTokens) != 2 or comandoRecebido != 'HELO'
-    while erro:
-        # envia mensagem de erro
-        sConexao.send('500 Syntax error, command unrecognized'.encode('UTF8'))
+    while not terminouConexao:
         # recebe mensagem
-        mensagemBytes = sConexao.recv(1024)
+        mensagemBytes = sConexao.recv(TAM_BUFFER_RECV)
         # decodifica bytes recebidos
-        mensagemTokens = mensagemBytes.decode('UTF8').split()
-        # pega comando
-        comandoRecebido = mensagemTokens[0]
-        # verifica inconsistencia e se o comando passado foi HELO <...>
-        erro = len(mensagemTokens) != 2 or comandoRecebido != 'HELO'
-    # adquire informacao
-    idCliente = mensagemTokens[1]
+        mensagem = mensagemBytes.decode('UTF8')
+        # separa em tokens
+        mensagemTokens = mensagem.split()
+        quantidadeTokens = len(mensagemTokens)
 
-    # 250 <...> {idCliente}<...> ===============================================
-    sConexao.send('250 Hello {}, pleased to meet you'.format(idCliente).encode('UTF8'))
+        if mensagem.startswith('HELO') and quantidadeTokens == 2 and not clienteIdentificado:
+            clienteIdentificado = True
+            idCliente = mensagemTokens[1]
+            sConexao.send('250 Hello {}, pleased to meet you'.format(idCliente).encode('UTF8'))
 
-    # TODO daqui para baixo executar em loop até receber quit
-    # MAIL FROM: <...> =====================================================
-    mensagemBytes = sConexao.recv(1024)
-    mensagemTokens = mensagemBytes.decode('UTF8').split()
-    try:
-        comandoRecebido = '{} {}'.format(mensagemTokens[0], mensagemTokens[1])
-    except Exception:
-        comandoRecebido = ''
-    erro = len(mensagemTokens) != 3 or comandoRecebido != 'MAIL FROM:'
-    while erro:
-        sConexao.send('500 Syntax error, command unrecognized'.encode('UTF8'))
-        mensagemBytes = sConexao.recv(1024)
-        mensagemTokens = mensagemBytes.decode('UTF8').split()
-        try:
-            comandoRecebido = '{} {}'.format(mensagemTokens[0], mensagemTokens[1])
-        except Exception:
-            comandoRecebido = ''
-        erro = len(mensagemTokens) != 3 or comandoRecebido != 'MAIL FROM:'
-    emailRemetente = mensagemTokens[2]
+        elif mensagem.startswith('MAIL FROM:') and quantidadeTokens == 3 and clienteIdentificado and ordemComando == 0:
+            ordemComando += 1
+            emailRemetente = mensagemTokens[2]
+            sConexao.send('250 {} Sender ok'.format(emailRemetente).encode('UTF8'))
 
-    # 250 {emailRemetente} <...> ===========================================
-    sConexao.send('250 {} Sender ok'.format(emailRemetente).encode('UTF8'))
+        elif mensagem.startswith('RCPT TO:') and quantidadeTokens == 3 and clienteIdentificado and ordemComando == 1:
+            # SECTION - caso o destinatario nao exista
+            # TODO - verificar se o existe a caixa de entrada do destinatario
+            # NOTE - caso o destinatário especificado não seja um dos usuários 
+            # do sistema, o servidor deverá responder com "550 Address unknown".
+            # NOTE - a conexão *não deve ser encerrada nesse caso*.
+            # NOTE - pular para o proximo email. (ordemComando = 0)
+            # !SECTION
+            ordemComando += 1
+            emailDestinatario = mensagemTokens[2]
+            sConexao.send('250 {} Recipient ok'.format(emailDestinatario).encode('UTF8'))
 
-    # RCPT TO: <...> =======================================================
-    mensagemBytes = sConexao.recv(1024)
-    mensagemTokens = mensagemBytes.decode('UTF8').split()
-    try:
-        comandoRecebido = '{} {}'.format(mensagemTokens[0], mensagemTokens[1])
-    except Exception:
-        comandoRecebido = ''
-    erro = len(mensagemTokens) != 3 or comandoRecebido != 'RCPT TO:'
-    while erro:
-        sConexao.send('500 Syntax error, command unrecognized'.encode('UTF8'))
-        mensagemBytes = sConexao.recv(1024)
-        mensagemTokens = mensagemBytes.decode('UTF8').split()
-        try:
-            comandoRecebido = '{} {}'.format(mensagemTokens[0], mensagemTokens[1])
-        except Exception:
-            comandoRecebido = ''
-        erro = len(mensagemTokens) != 3 or comandoRecebido != 'RCPT TO:'
-    emailDestinatario = mensagemTokens[2]
+        elif mensagem == 'DATA' and quantidadeTokens == 1 and clienteIdentificado and ordemComando == 2:
+            ordemComando += 1
+            sConexao.send('354 Enter mail, end with ".". on a line by itself'.encode('UTF8'))
+            # SECTION - receber email
+            _ = sConexao.recv(TAM_BUFFER_RECV) # STUB
+            # TODO - receber mensagens até receber uma linha com apenas '.' .
+            # !SECTION
+            mensagemRecebida = True
+            sConexao.send('250 Message accepted for delivery'.encode('UTF8'))
 
-    # 250 {emailDestinatario} <...> ========================================
-    sConexao.send('250 {} Recipient ok'.format(emailDestinatario).encode('UTF8'))
+        elif mensagem == 'QUIT' and quantidadeTokens == 1 and clienteIdentificado and mensagemRecebida and ordemComando == 0:
+            terminouConexao = True
+            sConexao.send('221 {} closing connection'.format(NOME_APRESENTACAO).encode('UTF8'))
+            # NOTE - Podemos fechar o socket aqui, por enquanto fech no loop do servidor
 
-    # DATA =================================================================
-    mensagemBytes = sConexao.recv(1024)
-    mensagemTokens = mensagemBytes.decode('UTF8').split()
-    comandoRecebido = mensagemTokens[0]
-    erro = len(mensagemTokens) != 1 or comandoRecebido != 'DATA'
-    while erro:
-        sConexao.send('500 Syntax error, command unrecognized'.encode('UTF8'))
-        mensagemBytes = sConexao.recv(1024)
-        mensagemTokens = mensagemBytes.decode('UTF8').split()
-        comandoRecebido = mensagemTokens[0]
-        erro = len(mensagemTokens) != 1 or comandoRecebido != 'DATA'
+        else:
+            sConexao.send('500 Syntax error, command unrecognized'.encode('UTF8'))
 
-    # 354 <...> ============================================
-    sConexao.send('354 Enter mail, end with ".". on a line by itself'.encode('UTF8'))
+        ordemComando %= 3
 
-    # <...> ================================================================
-    # TODO rebebimento do conteudo até até receber uma linha com apenas '.'
-
-    # 250 <...> ============================================
-    sConexao.send('250 Message accepted for delivery'.encode('UTF8'))
-
-
+# ==============================================================================
+# SECTION - entrada linha de comando
+# TODO - receber como argumento de linha de comando o nome de um arquivo e lê-lo.
+# NOTE - O arquivo conterá a lista de usuários do serviço de e-mail, um por linha.
+# NOTE - verificar se o arquivo com a lista de usuários foi corretamente passado como argumento. Qualquer erro na leitura desse processo (e.g., arquivo não existe, o formato não é o esperado) deverá gerar uma mensagem de erro e a execução deverá abortar.
+# !SECTION
 
 SERVIDOR_ENDERECO = '127.0.0.1'
 SERVIDOR_PORTA = 49152
